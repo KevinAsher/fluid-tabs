1b1cb550e71ff9b74b6fed100cb69e9c054e6996
Kevin Weiss <kev_asher@hotmail.com>
2022-07-25 16:19:06 -0300

    fix types and TS issues

--- packages/react-tab-flow/vite.config.ts
+++ packages/react-tab-flow/vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
+import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
