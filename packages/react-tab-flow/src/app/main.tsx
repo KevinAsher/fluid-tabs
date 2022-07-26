2db1033a8259b910d63b6b9a880d200708a5393e
Kevin Asher <kev_asher@hotmail.com>
2022-07-26 16:38:08 -0300

    copy demo to package for a better vite development experience, but production and performance testing still needs to be on the other subproject

--- packages/react-tab-flow/src/app/main.tsx
+++ packages/react-tab-flow/src/app/main.tsx
@@ -1,10 +1,22 @@
+import { CssBaseline } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
