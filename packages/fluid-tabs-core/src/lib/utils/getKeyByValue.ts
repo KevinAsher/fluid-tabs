05c99e5d627dca1fe7c8d51de77ba14965c0242a
Kevin Weiss <kev_asher@hotmail.com>
2024-06-08 14:08:09 -0300

    Bypass ts warning

--- packages/fluid-tabs-core/src/lib/utils/getKeyByValue.ts
+++ packages/fluid-tabs-core/src/lib/utils/getKeyByValue.ts
@@ -1,8 +1,9 @@
 export default function getKeyByValue(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   map: Map<any, number>,
   searchValue: string | number,
 ) {
-  for (let [key, value] of map.entries()) {
+  for (const [key, value] of map.entries()) {
     if (value === searchValue) return key;
   }
 }
